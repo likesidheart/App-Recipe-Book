4411131bde5ea3d65e87add613bc62e00e9a4005
Siddharth Thakor <sidht1295@gmail.com>
2018-08-13 19:19:18 -0400

    signin anf logout added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,46 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { SigninPage } from '../pages/signin/signin';
 import { SignupPage } from '../pages/signup/signup';
 import firebase from 'firebase';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tabsPage = TabsPage
+  rootPage:any = TabsPage
   signinPage = SigninPage
   signupPage = SignupPage
-  @ViewChild('nav') nav:NavController
+  isAuthenticated = false;
+  @ViewChild('nav') nav: NavController
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private menuCtrl:MenuController) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController,
+    private authService: AuthService) {
     firebase.initializeApp({
       apiKey: "AIzaSyBV0O4anpBSVnfqnRs0D0gdLT0qDDgT9yY",
       authDomain: "ionic-recipebook-40225.firebaseapp.com"
     })
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.rootPage = TabsPage
+      } else {
+        this.isAuthenticated = false
+        this.rootPage = SigninPage
+      }
+    })
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
-  onLoad(page:any) {
+  onLoad(page: any) {
     this.nav.setRoot(page);
     this.menuCtrl.close();
   }
   onLogout() {
-
+    this.authService.logout();
+    this.menuCtrl.close()
   }
 }
 
--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { NgForm } from '../../../node_modules/@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 
 @IonicPage()
@@ -10,9 +11,26 @@ import { NgForm } from '../../../node_modules/@angular/forms';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private authService:AuthService,private loadingCtrl:LoadingController,
+  private alertCtrl:AlertController) {
   }
   onSignin(form:NgForm) {
-    console.log(form.value)
+    let loading = this.loadingCtrl.create({
+      content:'Logging in.....'
+    })
+    loading.present();
+    this.authService.signin(form.value.email,form.value.password)
+    .then(data => {
+      loading.dismiss();
+    })
+    .catch(error => {
+      loading.dismiss();
+      let alert = this.alertCtrl.create({
+        title:'Signin failed!',
+        message: error.message,
+        buttons: ['Ok']
+      })
+      alert.present();
+    })
   }
 }
